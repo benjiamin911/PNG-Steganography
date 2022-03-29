3211b6351c3d382860ca0d57cceb9f2c5ed6e1b3
Kris2Wu <90416276+Kris2Wu@users.noreply.github.com>
2022-03-28 21:25:24 -0400

    3rd commit
    rewrite everything and add "openfile" function

--- PNG_tool/PNG_tool/PNG_tool.cpp
+++ PNG_tool/PNG_tool/PNG_tool.cpp
@@ -11,37 +11,142 @@ HINSTANCE hInst;                                // 当前实例
 WCHAR szTitle[MAX_LOADSTRING];                  // 标题栏文本
 WCHAR szWindowClass[MAX_LOADSTRING];            // 主窗口类名
 
-// 此代码模块中包含的函数的前向声明:
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
-BOOL CALLBACK DialogProc(
-    HWND hwndDlg,  // handle to dialog box			
-    UINT uMsg,     // message			
-    WPARAM wParam, // first message parameter			
-    LPARAM lParam  // second message parameter			
-);
+void DoFileOpen(HWND hwnd)
+{
+	HRESULT hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED |
+		COINIT_DISABLE_OLE1DDE);
+	if (SUCCEEDED(hr))
+	{
+		IFileOpenDialog* pFileOpen;
+
+		// Create the FileOpenDialog object.
+		hr = CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_ALL,
+			IID_IFileOpenDialog, reinterpret_cast<void**>(&pFileOpen));
+
+		if (SUCCEEDED(hr))
+		{
+			// Show the Open dialog box.
+			hr = pFileOpen->Show(NULL);
+
+			// Get the file name from the dialog box.
+			if (SUCCEEDED(hr))
+			{
+				IShellItem* pItem;
+				hr = pFileOpen->GetResult(&pItem);
+				if (SUCCEEDED(hr))
+				{
+					PWSTR pszFilePath;
+					hr = pItem->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath);
+
+					// Display the file name to the user.
+					if (SUCCEEDED(hr))
+					{
+						MessageBoxW(NULL, pszFilePath, L"File Path", MB_OK);
+						CoTaskMemFree(pszFilePath);
+					}
+					pItem->Release();
+				}
+			}
+			pFileOpen->Release();
+		}
+		CoUninitialize();
+	}
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+		case WM_CLOSE:
+			DestroyWindow(hwnd);
+			break;
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			break;
+		case WM_COMMAND:
+			switch (LOWORD(wParam))
+			{
+			case IDM_EXIT:
+				PostMessage(hwnd, WM_CLOSE, 0, 0);
+				break;
+			case IDM_OPEN:
+				DoFileOpen(hwnd);
+				break;
+			case IDM_SAVE:
+				//DoFileSave(hwnd);
+				break;
+			case IDM_ANALYZE:
+				//DoAnalyze(hwnd);
+				break;
+			case IDM_DELETE:
+				//DoDelete(hwnd);
+				break;
+			case IDM_INJECT:
+				//DoInject(hwnd);
+				break;
+			}
+			break;
+		default:
+			return DefWindowProc(hwnd, msg, wParam, lParam);
+		}
+		return 0;
+}
+
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
                      _In_ LPWSTR    lpCmdLine,
                      _In_ int       nCmdShow)
 {
-    DialogBox(
-        hInstance,  // handle to module
-        MAKEINTRESOURCE(IDD_PNGTOOL_DIALOG),   // dialog box template
-        NULL,      // handle to owner window
-        DialogProc  // dialog box procedure
-    );
+	WNDCLASSEX wc;
+	HWND hwnd;
+	MSG Msg;
 
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = 0;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wc.lpszMenuName = MAKEINTRESOURCE(IDC_PNGTOOL);
+	wc.lpszClassName = (LPCWSTR)L"myWindowClass";
+	wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	if (!RegisterClassEx(&wc))
+	{
+		MessageBox(NULL, (LPCWSTR)L"Window Registration Failed!", (LPCWSTR)L"Error!",
+			MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
+
+	hwnd = CreateWindowEx(
+		0,
+		(LPCWSTR)L"myWindowClass",
+		(LPCWSTR)L"PNG Tool -- all in one",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, 480, 320,
+		NULL, NULL, hInstance, NULL);
+
+	if (hwnd == NULL)
+	{
+		MessageBox(NULL, (LPCWSTR)L"Window Creation Failed!", (LPCWSTR)L"Error!",
+			MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
+
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+	while (GetMessage(&Msg, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&Msg);
+		DispatchMessage(&Msg);
+	}
+	return Msg.wParam;
 }
 
-BOOL CALLBACK DialogProc(
-    HWND hwndDlg,  // handle to dialog box			
-    UINT uMsg,     // message			
-    WPARAM wParam, // first message parameter			
-    LPARAM lParam  // second message parameter			
-)
-{
-    return false;
-}
+
+
+
--- PNG_tool/PNG_tool/resource.h
+++ PNG_tool/PNG_tool/resource.h
@@ -3,21 +3,13 @@
 // 供 PNG_tool.rc 使用
 //
 #define IDC_MYICON                      2
-#define IDD_PNGTOOL_DIALOG              102
-#define IDS_APP_TITLE                   103
-#define IDD_ABOUTBOX                    103
-#define IDM_ABOUT                       104
+#define IDC_PNGTOOL                     102
+#define IDM_OPEN			            103
+#define IDM_SAVE			            104
 #define IDM_EXIT                        105
-#define IDI_PNGTOOL                     107
-#define IDI_SMALL                       108
-#define IDC_PNGTOOL                     109
-#define IDR_MAINFRAME                   128
-#define IDC_BUTTON1                     1000
-#define IDC_BUTTON2                     1001
-#define IDC_BUTTON3                     1002
-#define IDC_BUTTON4                     1003
-#define IDC_BUTTON5                     1004
-#define IDC_STATIC                      -1
+#define IDM_ANALYZE			            106
+#define IDM_DELETE			            107
+#define IDM_INJECT			            108
 
 // Next default values for new objects
 // 
@@ -25,8 +17,8 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        129
-#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_COMMAND_VALUE         20000
 #define _APS_NEXT_CONTROL_VALUE         1005
-#define _APS_NEXT_SYMED_VALUE           110
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
